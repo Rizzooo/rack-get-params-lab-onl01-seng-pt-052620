de927ec719e669b842e2ca1d603457802060d113
Ronald Burrell Jr <ronburrell05@gmail.com>
2020-08-13 16:33:16 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -27,7 +27,7 @@ class Application
       
     elsif req.path.match(/add/)
       search_term = req.params["item"]
-      if @@items.include?(item)
+      if @@items.include?(search_term)
         @@cart << item
         "added #{item}"
       else 
